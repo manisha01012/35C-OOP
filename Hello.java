4f3348a0e75ac7103acf6e6f05bb094b8f4b1f30
manisha <gcmanisha87gmail.com>
2024-04-12 12:52:43 +0545

    Quoted

--- Hello.java
+++ Hello.java
@@ -9,5 +9,19 @@ public class Hello {
         I am on module ST4003CEM
         System.out.print (not println) and produces the same output. */
         System.out.print("This is my first program");
+        /*3. Write a program that prints the following output:
+
+        A "quoted" String is
+        'much' better if you learn
+        the rules of "escape sequences."
+
+        Also, "" represents an empty
+
+        String. Don't forget: use \"
+
+        instead of " !
+
+        '' is not the same as " */
+        System.out.print( "A quoted String is much better if you learn the rules of escape sequences");
     }
 }
